3d990180568d5358905959dd76a0a92d08e2eab6
John Burwell <john@b-wells.us>
2025-12-28 19:36:22 -0600

    Fix logging

--- custom_components/sunset_light/__init__.py
+++ custom_components/sunset_light/__init__.py
@@ -1,6 +1,7 @@
 
 """The Sunset Light integration."""
 import asyncio
+import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -9,6 +10,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Sunset Light from a config entry."""
+    _LOGGER = logging.getLogger(__name__)
     _LOGGER.info(
         "Setting up Sunset Light entry: id=%s title=%s data=%s",
         entry.entry_id,
