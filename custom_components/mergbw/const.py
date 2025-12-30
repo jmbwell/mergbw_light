848a41e2bd01f1e1f488f57de60068e99199e05e
John Burwell <john@b-wells.us>
2025-12-29 21:35:38 -0600

    Restore missing service UUID

--- custom_components/mergbw/const.py
+++ custom_components/mergbw/const.py
@@ -3,6 +3,7 @@
 DOMAIN = "mergbw"
 CONF_PROFILE = "profile"
 DEFAULT_PROFILE = "sunset_light"
+SERVICE_UUID = "0000fff0-0000-1000-8000-00805f9b34fb"
 # Hexagon-only services (will be no-ops for default profile)
 SERVICE_SET_SCENE_ID = "set_scene_id"
 SERVICE_SET_MUSIC_MODE = "set_music_mode"
